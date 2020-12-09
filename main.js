951d4b4acd05d82651ee86222fd569a7ce1cef42
Error404Danny <danny.lopez@loop.colum.edu>
2020-12-09 17:18:45 -0600

    Updates

---
+++ main.js
@@ -0,0 +1,12 @@
+$(".about").click(function(){
+
+    $(".about").css("background","white");
+})
+$(".gallery").click(function(){
+
+    $(".gallery").css("background","white");
+})
+$('#intro').hover(function(){
+
+    $("#intro").css("opacity", "1");
+})
